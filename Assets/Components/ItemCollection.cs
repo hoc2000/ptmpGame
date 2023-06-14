38c7616ae99748e3bbcef878e7a42bd5744bf698
hoc2000 <vutuhoc@gmail.com>
2023-06-14 17:25:33 +0700

    update file location

--- Assets/Components/ItemCollection.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemCollection.asset", menuName = "GameGuys/ItemCollection")]
-public class ItemCollection : ScriptableObject
-{
-    public List<Item> items;
-}
